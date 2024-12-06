ced2e83dc9d68b63d485ca2cc2ecfb21d29eae89
Changmin Kim <ksjchm4@gmail.com>
2024-12-06 13:56:42 +0900

    [Refact] stop.sh, start.sh 실행 권한 부여

---
+++ scripts/before_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "> Granting execute permissions for scripts"
+chmod +x /home/ubuntu/my-app/scripts/stop.sh
+chmod +x /home/ubuntu/my-app/scripts/start.sh
+
+echo "> Permissions granted successfully"
